b6242187997dbd7eadc291fcc5f402657b7cf950
OlegAndrus12 <andrusoleg123@gmail.com>
2022-04-24 14:05:56 +0300

    crud directory is not needed

--- crud/users.py
+++
@@ -1,8 +0,0 @@
-from db.mongodb import get_database
-
-
-async def get_users_list():
-    db = await get_database()
-    users_collection = await db.users.find().to_list(length=None)
-    print(users_collection)
-    return users_collection
