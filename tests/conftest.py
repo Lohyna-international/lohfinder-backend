83b1f667639f3a3dc71d7c1665feaf1928bcb0ae
Oleh Andrus <o.andrus@levi9.com>
2022-05-26 17:26:14 +0300

    fixing CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ import requests as re
 
 class Settings:
     API_V1_STR: str = "api/v1"
-    BACKEND_CORS_ORIGINS = (["http://localhost:8000", "https://remzjz.deta.dev/docs"],)
+    BACKEND_CORS_ORIGINS = ["http://localhost:8000", "https://remzjz.deta.dev/docs"]
     DEBUG = False
 
 
